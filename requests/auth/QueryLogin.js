92a3d5e063ef910fa0ed3c6f6ee0c0438e35d0e0
Artemij Kondurov <kondurovartemij@yandex.ru>
2025-09-02 22:34:57 +0300

    Trying change error with cookie

--- requests/auth/QueryLogin.js
+++ requests/auth/QueryLogin.js
@@ -133,8 +133,8 @@ module.exports = QueryLogin = async (ctx, connection) => {
         ctx.cookies.set('jwtToken', token, {
             expires: new Date(Date.now() + 604800000),
             httpOnly: true,
-            secure: true,
-            sameSite: 'None'
+            secure: isProduction,
+            sameSite: isProduction ? 'None' : 'Lax'
         });
         console.log('Cookies теперь работают)', isProduction, ctx.request.secure);
 
