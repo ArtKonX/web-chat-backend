b8fb51248921b1f7b8751a450b2da0aed95b7746
Artemij Kondurov <kondurovartemij@yandex.ru>
2025-09-03 17:23:14 +0300

    Trying change error with cookies in safari

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use(koaBody({
 
 // CORS middleware
 app.use(async (ctx, next) => {
-  const origin = 'http://localhost:3000' || ctx.request.headers.origin;
+  const origin = ctx.request.headers.origin || 'https://localhost:3000';
 
   ctx.set('Access-Control-Allow-Origin', origin);
   ctx.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
